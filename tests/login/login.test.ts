4a45d12afa99da5d6ef9a4b81b4eef1bf7dc02e6
mertakdemir <akdemirmertsafa@gmail.com>
2025-04-04 22:16:50 +0200

    first itteration

--- tests/login/login.test.ts
+++ tests/login/login.test.ts
@@ -10,20 +10,22 @@ test.describe('login form tests', () => {
     const existingUser = existingUsers[0]
 
     await page
-      .locator('#root form div:nth-child(1) > div > input')
+      .locator('#email') //Instead of using long locators that starts with "root..", I prefered to use "id" (email) as an locator
       .pressSequentially(existingUser.email)
 
     await page
-      .locator('#root form div:nth-child(2) > div > input')
+      .locator('#password')//Instead of using long locators that starts with "root..", I prefered to use "id" (password) as an locator
       .pressSequentially(existingUser.password)
 
-    // Submit button
-    const button = page.locator('form .MuiButton-sizeMedium')
-    // Click on the button
-    button.click()
+
+      const button = page.locator('form .MuiButton-sizeMedium')
+      // Click on the button
+      button.click()
 
     // Wait for 1 second until page is fully loaded
-    await page.waitForTimeout(1000)
+    //await page.waitForTimeout(1000)
+    await page.waitForSelector('text=Log out', { visible: true });// Having a hard wait in a code might cause unacceptable issues. Log out message might appear in a 05. milisecond for example but in this case we would wait 0.5 second more.
+                                                                  //Having {visible: true} will avoid the time waste
     await expect(page.getByText('Log out')).toBeVisible()
   })
 })
